fac03446f005e91b673af9f1d12d4bde67e1b03b
Mada Gower <gmgower@gmail.com>
2019-09-10 22:03:07 -0500

    Add useDarkMode to Navbar.jsx.

--- src/components/hooks/useLocalStorage.jsx
+++ src/components/hooks/useLocalStorage.jsx
@@ -5,13 +5,15 @@ const useLocalStorage = (key, initialValue) => {
     const [storedValue, setStoredValue] = useState(() => {
 
         const item = window.localStorage.getItem(key);
-        return item ? JSON.parse(item) : initialValue;
 
-        const setValue = value => {
-            setStoredValue(value);
-            localStorage.setItem(key, JSON.stringify(value));
-        }
+        return item ? JSON.parse(item) : initialValue;
     });
+    const setValue = value => {
+        setStoredValue(value);
+        localStorage.setItem(key, JSON.stringify(value));
+    };
 
     return [storedValue, setValue];
 }
+
+export default useLocalStorage;
